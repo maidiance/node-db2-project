0a89e9c96a13d1a99fbf54bb372dfccc4e1dcbf1
Winifred Lu <winnie.lu@comcast.net>
2022-02-15 14:27:24 -0500

    basic model complete, middleware in progress

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,11 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config');
+
+const checkCarId = async(req, res, next) => {
+  const { id } = req.params;
+  const car = await db('cars').where('id', id).first();
+  if(car === []) {
+    res.status(404).json({message: `car with id ${id} is not found`});
+  }
 }
 
 const checkCarPayload = (req, res, next) => {

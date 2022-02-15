df77363dcbda9d4e167ec5311ee030378dcf844b
Winifred Lu <winnie.lu@comcast.net>
2022-02-15 15:24:21 -0500

    small debug fix for server using json

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const server = express()
 
 // DO YOUR MAGIC
+server.use(express.json());
 const carsRouter = require('./cars/cars-router');
 server.use('/api/cars', carsRouter);
 

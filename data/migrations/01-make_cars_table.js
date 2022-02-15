f744c9be926a7080893ac08f01c8439f528e8973
Winifred Lu <winnie.lu@comcast.net>
2022-02-15 14:12:46 -0500

    basic make table complete

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl.string('vin', 128).unique().notNullable();
+    tbl.string('make', 128).notNullable();
+    tbl.string('model', 128).notNullable();
+    tbl.decimal('mileage', 128).notNullable();
+    tbl.string('title');
+    tbl.string('transmission');
+  });
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
